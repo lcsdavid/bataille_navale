48e4ebaeac7f9a457c64091fd9e4c0ab4a5b776c
lcsdavid <david-lucas-69@hotmail.fr>
2017-04-14 10:29:38 +0200

    Changements

--- jeu/fonction.php
+++ jeu/fonction.php
@@ -3,18 +3,21 @@
 function login($id, $pwd)
 {
     global $connexion;
-    $result = mysqli_query($connexion, "SELECT * FROM Joueur WHERE email = .$id. AND mdp = MD5(.$pwd.)");
-    if ($result) {
-        $array = $result->fetch_array();
+    $result = mysqli_query($connexion, "SELECT username FROM Joueur WHERE email = '".$id."' AND mdp = '".md5($pwd)."'");
+    if ($result->num_rows == 1) {
+        session_start();
+        $_SESSION['username'] = $result->fetch_field();
+        $_SESSION['timestamp'] = time();
     } else {
-        return false;
+        // Erreur
     }
 }
 
 function register($email, $pseudo, $name, $firstname, $gender, $birth, $town, $pwd)
 {
     global $connexion;
-    mysqli_query($connexion, "INSERT INTO Joueur (email, pseudonyme, nom, prenom, sexe, naissance, ville, mdp) VALUES ('".$email."','".$pseudo."','".$name."','".$firstname."','".$gender."','".$birth."','".$town."','".$pwd."')");
+    if (mysqli_query($connexion, "SELECT * FROM Joueur WHERE email = '".$email."'")->num_rows == 0);
+    mysqli_query($connexion, "INSERT INTO Joueur (email, pseudonyme, nom, prenom, sexe, naissance, ville, mdp) VALUES ('".$email."','".$pseudo."','".$name."','".$firstname."','".$gender."','".$birth."','".$town."','".md5($pwd)."')");
 }
 
 ?>
