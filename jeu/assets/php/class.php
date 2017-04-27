7261a76b3195cafee32b7febf877464c8e6724a6
lcsdavid <david-lucas-69@hotmail.fr>
2017-04-27 14:05:45 +0200

    Commit

--- jeu/assets/php/class.php
+++ jeu/assets/php/class.php
@@ -3,36 +3,40 @@
 class Grid
 {
     private $array = [""];
+    private $id_joueur;
+    private $id_partie;
+
+    function Grid() {
+        $this->init()->load();
+    }
 
     public function init()
     {
         for ($row = 'A'; $row <= 'J'; $row++) {
             for ($column = 1; $column <= 10; $column++) {
-                printf($row);
-                printf($column);
                 $cell = $row . (string)$column;
-                $array[$cell] = "Vide.";
+                $this->array[$cell] = "sea";
             }
         }
+        return $this;
     }
 
-    public function reload()
+    public function load()
     {
+        global $connexion;
+        $rset = mysqli_query($connexion,"SELECT * ");
 
     }
 
     public function display()
     {
-        $old_key = "";
-        foreach ($this->array as $key => $value) {
-            if ($old_key == $key)
-                echo "<tr><td class='cell coord'>" . $key . "</td>";
-            echo "<td class='cell " . $value . "'><a href='#" . $key . "'></a></td>";
-            if ($old_key == $key) {
-                echo "</tr>";
-                $old_key = $key;
+        for ($row = 'A'; $row <= 'J'; $row++) {
+            echo "<tr><td class='cell coord'>" . $row . "</td>";
+            for ($column = 1; $column <= 10; $column++) {
+                $cell = $row . (string)$column;
+                echo "<td class='cell " . $this->array[$cell] . "'><a href='#" . $cell . "'></a></td>";
             }
         }
+        return $this;
     }
-
 }
